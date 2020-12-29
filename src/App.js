f76c957269b830d94e3aa30271626fbef33ef6ef
Ankit Mishra <ankitm3114@gmail.com>
2020-12-29 13:44:01 +0530

    remove unused variables

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link, Route, Switch } from "react-router-dom";
+import { Route } from "react-router-dom";
 import Home from './views/Home';
 import Sources from './views/Sources';
 import SourceDetails from './views/SourceDetails';
